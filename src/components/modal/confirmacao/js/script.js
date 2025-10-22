be4f187b10fb9b83e57a3ce59c8e0e035028fee7
marimendonca1712 <mariana.decassia@hotmail.com>
2025-10-21 22:04:30 -0300

    Caixa de sugestões
    Foi colocada a caixa de justificativa. Falta alinhar com os outras caixas.

--- src/components/modal/confirmacao/js/script.js
+++ src/components/modal/confirmacao/js/script.js
@@ -1,5 +1,24 @@
-const openModalButton = document.getElementById("open-modal");
 
-openModalButton.addEventListener("click", () => {
-    window.location.href = "../avaliacao/index.html";
+
+const modal = document.getElementById("modal");
+const openBtn = document.getElementById("botao-confirm");
+const closeBtn = document.getElementById("close-modal");
+const confirmBtn = document.querySelector("confirmar-conclusao");
+
+openBtn.addEventListener("click", ()=> {
+    modal.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", ()=> {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
+confirmBtn.addEventListener("click", () => {
+    window.location.href = "avaliacao.html";
 });
