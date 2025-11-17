4e7923c7f4ff77fde86d54cc9520c6a4fdb061d3
João Pedro <59922786+joaoteodev@users.noreply.github.com>
2025-11-17 10:06:50 -0300

    test

--- src/assets/scripts/registerUsers.js
+++ src/assets/scripts/registerUsers.js
@@ -1,9 +1,5 @@
-async function loadUsers() {
-  if (!localStorage.getItem("users")) {
-    const response = await fetch("db/users.json");
-    const users = await response.json();
-    localStorage.setItem("users", JSON.stringify(users));
-  }
-}
+import users from "/db/users.json" with { type: "json" };
 
-loadUsers();
+if (!localStorage.getItem("users")) {
+  localStorage.setItem("users", JSON.stringify(users));
+}
