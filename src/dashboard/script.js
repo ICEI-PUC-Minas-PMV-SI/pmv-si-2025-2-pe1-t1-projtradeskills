87c977b4bfb467970fe58761ac948392641f8576
Raíssa Nepomuceno <raissanepomuceno8@gmail.com>
2025-11-20 18:08:55 -0300

    feat: dashboard new responsive style
    Alteração no design do Dashboard e adicionando novo menu sidebar.

---
+++ src/dashboard/script.js
@@ -0,0 +1,13 @@
+/*document.addEventListener('DOMContentLoaded', function() {
+    // Capturando o elemento de modal
+    const creditsUnlockModal = document.getElementById('modal-credits-unlock');
+
+    // Check if the browser supports the <dialog> element's showModal method
+    if (creditsUnlockModal && typeof creditsUnlockModal.showModal === 'function') {
+        creditsUnlockModal.showModal(); // Show the modal
+    } else if (creditsUnlockModal) {
+        // Fallback for older browsers or if using a custom modal implementation
+        // For example, if you're using CSS to hide/show the modal
+        creditsUnlockModal.style.display = 'block'; 
+    }
+});*/
